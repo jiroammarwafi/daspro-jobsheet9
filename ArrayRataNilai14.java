6f686c90ab3121fae294a165783bf13f4571d735
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2024-11-02 00:09:32 +0700

    Commit kedua

--- ArrayRataNilai14.java
+++ ArrayRataNilai14.java
@@ -2,18 +2,33 @@ import java.util.Scanner;
 public class ArrayRataNilai14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0, rata2;
+        int[] nilaiMhs; // deklarasi array
+        double[] total = new double [2];
+        double[] rata2 = new double [2];
+        int jmlLulus=0, jmlTdkLulus=0;
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jmlMhs = sc.nextInt(); // meminta input
+        nilaiMhs = new int [jmlMhs]; // inisialisasi array dengan kapasitas menyesuaikan var. jmlMhs
 
         for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
         }
-        for (int i = 0; i < nilaiMhs.length; i++){
-            total += nilaiMhs[i];
+        for (int i=0; i<nilaiMhs.length; i++){
+            if (nilaiMhs[i] > 70) { // menggunakan indeks 0 untuk menyimpan nilai mahasiswa yang lulus
+                jmlLulus++; // menambah dan menyimpan jumlah mahasiswa yang lulus
+                total[0] += nilaiMhs[i];
+                rata2[0] = total [0]/jmlLulus;
+            }
+            else { // menggunakan indeks 1 untuk menyimpan nilai mahasiswa yang tidak lulus
+                jmlTdkLulus++;
+                total[1] += nilaiMhs[i];
+                rata2[1] = total [1]/jmlTdkLulus;
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " +rata2);
+        System.out.println("Rata-rata nilai lulus = " + rata2 [0]);
+        System.out.println("Rata-rata nilai tidak lulus = " + rata2 [1]);
     sc.close();
     }
 }
