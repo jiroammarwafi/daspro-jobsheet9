0cb12e5db0ed6d5ec3716169cfb2eff67af53026
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2024-11-01 17:26:55 +0700

    Commit kedua

--- ArrayNilai14.java
+++ ArrayNilai14.java
@@ -4,13 +4,18 @@ public class ArrayNilai14 {
         Scanner sc = new Scanner(System.in);
         //set limit nilai variabel menjadi 10
         int[] nilaiAkhir = new int[10];
-        // loop untuk me
-        for (int i = 0; i<10; i++){
+        // loop untuk meminta input pengguna sebanyak 10 kali, length = panjang, alternatif untuk menyesuaikan dengan limit array pada variabel nilai akhir
+        for (int i = 0; i < nilaiAkhir.length; i++){
             System.out.print("Masukkan nilai akhir ke-" +i+" : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i=0; i<10; i++){
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        // seleksi kelulusan
+        for (int i=0; i<nilaiAkhir.length; i++){
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
         }
         sc.close();
     }
